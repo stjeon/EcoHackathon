fb5fd8e63c500568eff78ca8f6dc6f8daf768103
Stephen Jeon <jeons99@gmail.com>
2018-02-03 15:32:30 -0500

    added getuser code

--- server.js
+++ server.js
@@ -5,14 +5,10 @@ var bodyParser = require('body-parser');
 var cfenv = require("cfenv")
 var pg = require('pg')
 
-const conString =process.env.ELEPHANTSQL_URL ||  'postgres://szwruwjt:JxoAgI47YW2UgHw1hn5gMLrVsPc7rs_0@elmer.db.elephantsql.com'
+const conString =process.env.ELEPHANTSQL_URL ||  'postgres://szwruwjt:JxoAgI47YW2UgHw1hn5gMLrVsPc7rs_0@elmer.db.elephantsql.com:5432/szwruwjt'
 
 var client= new pg.Client(conString);
-client.connect(function(err){
-    if(err){
-        return console.error('coult not connect', err);
-    }else {return console.log('SUCCESS')}
-})
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -26,7 +22,7 @@ var appEnv = cfenv.getAppEnv()
 // start the server on the given port and binding host, and print
 // url to server when it starts
 
-app.listen(appEnv.port, '0.0.0.0', function() {
+app.listen('6002', '0.0.0.0', function() {
     console.log("server starting on " + appEnv.url)
 });
 
@@ -35,8 +31,19 @@ app.get('/', function(req, res){
   res.send('HI');
 });
 
-app.get('/home/GetUser', function(req,res){
+app.get('/Login/GetUser', function(req,res){
+    client.connect(function(err){
+        if(err)
+            return console.error('could not connect', err);
 
+        client.query("SELECT COUNT(*) from accounts where userid = $1 AND password = $2",
+            [req.query.username, req.query.password], function(err, results){
+            if(results.rows[0].count == 0)
+                res.json({value: false});
+            else
+                res.json({value: true});
+            })
+    })
 });
 
 app.post('/register', function (req, res){
