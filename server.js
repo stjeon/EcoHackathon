494a998dc1434dec934373743548ced4807d238c
Stephen Jeon <jeons99@gmail.com>
2018-02-03 14:41:38 -0500

    added connection

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ var express = require('express')
 var app = express()
 var cors = require('cors');
 var bodyParser = require('body-parser');
-
 var cfenv = require("cfenv")
+var pg = require('pg')
 
+const conString =process.env.ELEPHANTSQL_URL ||  'postgres://szwruwjt:JxoAgI47YW2UgHw1hn5gMLrVsPc7rs_0@elmer.db.elephantsql.com'
+
+var client= new pg.Client(conString);
+client.connect(function(err){
+    if(err){
+        return console.error('coult not connect', err);
+    }else {return console.log('SUCCESS')}
+})
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -26,3 +34,13 @@ app.get('/', function(req, res){
   console.log('CALLED')
   res.send('HI');
 });
+
+app.get('/home/GetUser', function(req,res){
+
+});
+
+app.post('/register', function (req, res){
+
+});
+
+
